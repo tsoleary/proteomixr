68ec3b0cd632b8805a904a6e16aa2b55c2d2e385
tsoleary <tsoleary12@gmail.com>
2019-01-30 16:39:42 -0500

    add function test

---
+++ R/as_group.R
@@ -0,0 +1,19 @@
+#' Function applied to each peptide for a group of data with median as default
+#'
+#' This function allows you to express your love of cats.
+#' @param dat data frame with peptide level data as rows and samples in columns
+#' @param col a numeric vector containing the column numbers of the group
+#' that the function will be performed on
+#' @keywords group
+#' @export
+#' @examples
+#' as_group(df, c(4:6))
+
+as_group <- function (dat, col, FUN = median){
+  list <- NULL
+  for (i in 1:nrow(dat)){
+    temp <- FUN(as.numeric(dat[i, col], na.rm = TRUE))
+    list <- c(list, temp)
+  }
+  return(list)
+}
